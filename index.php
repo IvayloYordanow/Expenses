6761d48ea83e4df0a5c144f5982acba876edb631
IvayloYordanow <i.yordanow@gmail.com>
2013-09-25 12:43:27 +0300

    Update index.php

--- index.php
+++ index.php
@@ -30,6 +30,9 @@ require 'Array.php';
                    else {
                        $selected='';
                    }
+                   
+                   
+                   
                   echo '<option '. $selected. ' value= "'.$key.'">'.$value.'</option>';
                }
                
@@ -37,7 +40,7 @@ require 'Array.php';
            </select>
                <input type="submit" value="Filter"/>
             
-        </div>
+          </div>
     </form>
     <br>
    
@@ -54,6 +57,7 @@ require 'Array.php';
             
             
             <?php
+            $sum=0;
             if(file_exists('data.txt')){
                 
                 $result=  file('data.txt');
@@ -77,7 +81,7 @@ require 'Array.php';
                                                  <td>'.$columns[2].'</td>
                                                  <td>'.$Type[trim($columns[3])].'</td>    
                                                  </tr>';
-      
+      $sum+= (float)$columns[2];
                                      }   
                                      
                }
@@ -87,14 +91,12 @@ require 'Array.php';
 
 ?>
                     
-                
-                    
-                   
-            
-            
-            
-            
-            
+         	<tr>
+<td>  </td>
+<td>  </td>
+<td><?php echo $sum ?></td>
+<td>  </td>
+</tr>         
             
             
         </table>
